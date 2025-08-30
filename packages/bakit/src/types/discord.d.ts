00bf741474a6bcfb445da1986abf549bfc3b7501
Louis Johnson <louiszn269@gmail.com>
2025-08-30 11:29:30 +0700

    chore: add note

--- packages/bakit/src/types/discord.d.ts
+++ packages/bakit/src/types/discord.d.ts
@@ -1,7 +1,11 @@
+/**
+ * This file is used to redeclare original client to the custom one
+ * Most of the structure is from Base, but some might be not
+ */
 import { BakitClient } from "../BakitClient.ts";
 
 declare module "discord.js" {
-	interface Message {
+	interface Base {
 		client: BakitClient<true>;
 	}
 }
