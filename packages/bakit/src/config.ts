e179c5bdfdbfc848ebed100fa1eedab271f45345
Louis Johnson <louiszn269@gmail.com>
2025-10-10 15:49:49 +0700

    feat: enhance config loading

--- packages/bakit/src/config.ts
+++ packages/bakit/src/config.ts
@@ -1,8 +1,9 @@
 import { ClientOptions, GatewayIntentBits } from "discord.js";
 import { z } from "zod";
 
-import { join } from "node:path";
 import { pathToFileURL } from "node:url";
+
+import glob from "tiny-glob";
 
 const ProjectConfigSchema = z.object({
 	intents: z
@@ -14,24 +15,61 @@ const ProjectConfigSchema = z.object({
 export type ProjectConfigInput = z.input<typeof ProjectConfigSchema>;
 export type ProjectConfig = z.output<typeof ProjectConfigSchema>;
 
-export function defineConfig(config: ProjectConfigInput): ProjectConfig {
-	return ProjectConfigSchema.parse(config);
+/**
+ * Define config object for your project. This is just a cleaner way to define config.
+ * @param config The partial version of project config.
+ * @returns The same config you provided earlier.
+ */
+export function defineConfig(config: ProjectConfigInput): ProjectConfigInput {
+	return config;
 }
 
 let _config: ProjectConfig | undefined;
 
-export async function loadConfig(): Promise<ProjectConfig> {
-	const configPath = pathToFileURL(join(process.cwd(), "bakit.config.ts")).toString();
+/**
+ * Load the config file and save them for later usage.
+ * @param cwd The location of the config file, uses root by default.
+ * @returns The complete config with default values from the validation.
+ */
+export async function loadConfig(cwd = process.cwd()): Promise<ProjectConfig> {
+	if (_config) {
+		console.warn("loadConfig() was called more than once. This shouldn't happen.");
+		return _config;
+	}
 
-	const { default: config } = (await import(configPath)) as {
+	// Support multiple config file extensions
+	// The order of getting config is from left to right
+	const extensions = ["ts", "js"];
+	const globPattern = `bakit.config.{${extensions.join(",")}}`;
+
+	const [configPath, other] = await glob(globPattern, {
+		cwd: cwd.replace(/\\/g, "/"), // ensure the path uses `/` instead of `\` on Windows
+		absolute: true,
+	});
+
+	if (!configPath) {
+		throw new Error("Missing config file");
+	}
+
+	if (other) {
+		console.warn(`Multiple config files found in ${cwd}. Using ${configPath}.`);
+	}
+
+	const configFileURL = pathToFileURL(configPath).toString();
+
+	const { default: config } = (await import(configFileURL)) as {
 		default: ProjectConfig;
 	};
 
-	_config = config;
+	_config = Object.freeze(await ProjectConfigSchema.parseAsync(config));
 
-	return config;
+	return _config;
 }
 
+/**
+ * Get the loaded config of the project.
+ * @returns The project config.
+ */
 export function getConfig(): ProjectConfig {
 	if (!_config) {
 		throw new Error("Project config is not loaded.");
