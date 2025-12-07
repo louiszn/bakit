e0a8c856302f8c1fc8704604e17c0ae6e47c3a68
Louis Johnson <louiszn269@gmail.com>
2025-12-07 16:16:46 +0700

    fix: add user to Params

--- packages/bakit/src/base/command/param/Params.ts
+++ packages/bakit/src/base/command/param/Params.ts
@@ -1,4 +1,4 @@
-import { NumberParam, StringParam } from "./Param.js";
+import { NumberParam, StringParam, UserParam } from "./Param.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function createFactory<Args extends any[], Instance>(ctor: ConstructorLike<Args, Instance>) {
@@ -8,4 +8,5 @@ function createFactory<Args extends any[], Instance>(ctor: ConstructorLike<Args,
 export const Params = {
 	string: createFactory(StringParam),
 	number: createFactory(NumberParam),
+	user: createFactory(UserParam),
 } as const;
