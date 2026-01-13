eaa6219d26cda7027a3e48a501acb99e57de9fc8
Louis Johnson <louiszn269@gmail.com>
2026-01-13 09:24:56 +0700

    fea(utils)t: add tests

--- packages/utils/src/promise.ts
+++ packages/utils/src/promise.ts
@@ -1,14 +1,35 @@
 import type { FunctionLike, Promisify } from "./types/index.js";
 
+/**
+ * Promisify a value.
+ *
+ * If the value is a function, it will be wrapped in an async function
+ * that calls the original function with the given arguments.
+ * If the value is not a function, it will be wrapped in a resolved promise.
+ *
+ * @param target - The value to be wrapped.
+ * @returns A promise that resolves to the return value of the target function, or the target value itself if it is not a function.
+ */
 export function promisify<T>(target: T): Promisify<T> {
 	if (typeof target === "function") {
-		const fn: FunctionLike = async (...args: unknown[]) => await target(...args);
+		const fn: FunctionLike = async function (this: unknown, ...args) {
+			return await target.apply(this, args);
+		};
+
 		return fn as Promisify<T>;
 	}
 
 	return Promise.resolve(target) as Promisify<T>;
 }
 
+/**
+ * Checks if a value is a promise-like object.
+ *
+ * A promise-like object is defined as having a `then` property that is a function.
+ *
+ * @param value - The value to check.
+ * @returns True if the value is a promise-like object, false otherwise.
+ */
 export function isPromiseLike(value: unknown): value is PromiseLike<unknown> {
 	if (typeof value !== "object" || value === null) {
 		return false;
@@ -17,6 +38,14 @@ export function isPromiseLike(value: unknown): value is PromiseLike<unknown> {
 	return "then" in value && typeof value.then === "function";
 }
 
+/**
+ * Sleeps for a given duration and returns a promise that resolves when the sleep is over.
+ * If an AbortSignal is provided, the promise will be rejected with an AbortError if the signal is aborted during the sleep.
+ *
+ * @param duration The duration to sleep for in milliseconds.
+ * @param signal An AbortSignal to listen for abort events.
+ * @returns A promise that resolves when the sleep is over, or rejects with an AbortError if the signal is aborted during the sleep.
+ */
 export function sleep(duration: number, signal?: AbortSignal) {
 	return new Promise<void>((resolve, reject) => {
 		const cancel = () => reject(new DOMException("Aborted", "AbortError"));
