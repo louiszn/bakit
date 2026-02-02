52a2450caeecc9a0b7a77c1111f3deb0153ac4e6
Louis Johnson <louiszn269@gmail.com>
2026-02-02 23:17:19 +0700

    feat(shard)!: reimplement shard

--- packages/gateway/tsup.config.ts
+++ packages/gateway/tsup.config.ts
@@ -1,11 +1,5 @@
 import { makeConfig } from "../../tsup.config.js";
 
-export default makeConfig(
-	{
-		entry: ["src/index.ts"],
-	},
-	{
-		entry: ["src/services/*.ts"],
-		outDir: "dist/services",
-	},
-);
+export default makeConfig({
+	entry: ["src/index.ts"],
+});
