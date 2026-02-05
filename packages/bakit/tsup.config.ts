cdc8fb7803c889e49be4acf6c7e165f1e4eaac09
Louis Johnson <louiszn269@gmail.com>
2026-02-05 22:54:09 +0700

    chore(core): clean up config

--- packages/bakit/tsup.config.ts
+++ packages/bakit/tsup.config.ts
@@ -1,13 +1,5 @@
 import { makeConfig } from "../../tsup.config.js";
 
-export default makeConfig(
-	{
-		entry: ["src/index.ts"],
-	},
-	{
-		entry: {
-			cli: "src/lib/cli/index.ts",
-		},
-		dts: false,
-	},
-);
+export default makeConfig({
+	entry: ["src/index.ts"],
+});
