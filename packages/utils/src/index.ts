a135b4b812a19fb49117e4c7066cd9434c8a549f
Louis Johnson <louiszn269@gmail.com>
2026-02-10 21:35:48 +0700

    feat: add snowflake util

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,6 +2,7 @@ export * from "./string.js";
 export * from "./promise.js";
 export * from "./object.js";
 export * from "./module.js";
+export * from "./snowflake.js";
 
 export { Collection, type ReadonlyCollection } from "@discordjs/collection";
 export { default as Queue, type Options as QueueOptions, type QueueAddOptions } from "p-queue";
