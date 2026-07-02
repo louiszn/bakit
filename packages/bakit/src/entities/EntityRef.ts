b086db14dc432b72a07883e3fd7c642676876d9b
Louis <git@louiszn.fyi>
2026-07-02 17:21:41 +0700

    feat: add EntityRef

---
+++ packages/bakit/src/entities/EntityRef.ts
@@ -0,0 +1,58 @@
+export interface EntityRef<TSnapshot> {
+	readonly id: string;
+
+	fetch(): Promise<TSnapshot>;
+
+	get(required: true): Promise<TSnapshot>;
+	get(required?: false): Promise<TSnapshot | undefined>;
+	get(required?: boolean): Promise<TSnapshot | undefined>;
+
+	resolve(required: true): Promise<TSnapshot>;
+	resolve(required?: false): Promise<TSnapshot | undefined>;
+	resolve(required?: boolean): Promise<TSnapshot | undefined>;
+}
+
+export abstract class BaseEntityRef<TSnapshot> implements EntityRef<TSnapshot> {
+	abstract readonly id: string;
+
+	protected abstract _fetch(): Promise<TSnapshot>;
+	protected abstract _get(): Promise<TSnapshot | undefined>;
+
+	fetch(): Promise<TSnapshot> {
+		return this._fetch();
+	}
+
+	get(required: true): Promise<TSnapshot>;
+	get(required?: false): Promise<TSnapshot | undefined>;
+	get(required?: boolean): Promise<TSnapshot | undefined>;
+	async get(required?: boolean): Promise<TSnapshot | undefined> {
+		const snapshot = await this._get();
+
+		if (snapshot === undefined && required) {
+			throw new Error(`Entity ${this.id} could not be found`);
+		}
+
+		return snapshot;
+	}
+
+	resolve(required: true): Promise<TSnapshot>;
+	resolve(required?: false): Promise<TSnapshot | undefined>;
+	resolve(required?: boolean): Promise<TSnapshot | undefined>;
+	async resolve(required?: boolean): Promise<TSnapshot | undefined> {
+		const cached = await this.get();
+
+		if (cached !== undefined) {
+			return cached;
+		}
+
+		try {
+			return await this.fetch();
+		} catch (error) {
+			if (required) {
+				throw error;
+			}
+
+			return undefined;
+		}
+	}
+}
--- packages/bakit/src/index.ts
+++ packages/bakit/src/index.ts
@@ -1,4 +1,5 @@
 export { Collection } from "@discordjs/collection";
 export { REST } from "@discordjs/rest";
-
 export { GatewayIntentBits, Routes } from "discord-api-types/v10";
+
+export * from "./entities";
