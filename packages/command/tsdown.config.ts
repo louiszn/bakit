933597b34136d9375fbc64d14d0319cbfa48cd86
Louis <git@louiszn.fyi>
2026-07-28 15:49:49 +0700

    chore: init @bakit/command

---
+++ packages/command/tsdown.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsdown";
+
+import rootConfig from "../../tsdown.config";
+
+// biome-ignore lint/style/noDefaultExport: tsdown uses default export for main config
+export default defineConfig({
+	...rootConfig,
+	entry: ["src/index.ts"],
+});
