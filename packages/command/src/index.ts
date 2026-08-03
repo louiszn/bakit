2b3314efc984fbdb1dafc5bdccdd1892fc330620
Louis <git@louiszn.fyi>
2026-08-03 18:08:37 +0700

    chore: remove duplicate export

--- packages/command/src/index.ts
+++ packages/command/src/index.ts
@@ -1,6 +1,4 @@
 export * from "./command";
-export * from "./command/CommandRegistry";
-export * from "./command/parser";
 export * from "./config";
 export * from "./context";
 export * from "./errors";
